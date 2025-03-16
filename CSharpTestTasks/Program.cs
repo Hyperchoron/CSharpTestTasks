a8265f3bc80b2c99e2578623d26acda92a66d067
Matvey Losev <tutorial.l2003@gmail.com>
2025-03-16 14:11:31 +0300

    Completed the first task.

--- CSharpTestTasks/Program.cs
+++ CSharpTestTasks/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string s = Console.ReadLine() ?? "";
+
+            if ((s.Length & 1) == 0) // Количество чётное.
+            {
+                int substringLength = s.Length >> 1;
+
+                string firstSubString = new string(s.Substring(0, substringLength).Reverse().ToArray());
+                string secondSubString = new string(s.Substring(substringLength, substringLength).Reverse().ToArray());
+
+                Console.WriteLine(firstSubString + secondSubString);
+            }
+            else
+            {
+                string reversedString = new string(s.Reverse().ToArray());
+
+                Console.WriteLine(reversedString + s);
+            }
         }
     }
 }
