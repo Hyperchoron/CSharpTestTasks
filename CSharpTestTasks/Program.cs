0c5f6c4651176a4105dfc4727447b05833792079
Matvey Losev <tutorial.l2003@gmail.com>
2025-03-16 14:08:02 +0300

    Add project files.

---
+++ CSharpTestTasks/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpTestTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
